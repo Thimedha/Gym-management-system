9ad762b7518f744bec0397cc82637009c2628699
Thimedha Viraj <153438859+Thimedha@users.noreply.github.com>
2024-05-17 02:28:04 +0530

    Add files via upload

---
+++ included/Header.php
@@ -0,0 +1,26 @@
+<?php
+
+@include './config.php';
+
+session_start();
+
+if(!isset($_SESSION['admin_name'])){
+   header('location:./login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Boxicons -->
+	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+	<link rel="stylesheet" href="https://docs.fontawesome.com/">
+	<!-- My CSS -->
+	<link rel="stylesheet" href="./css/style.css">
+	<link rel="stylesheet" href="./css/btn.css">
+	<title>Body Tone Center</title>
+</head>
---
+++ included/navContainer.php
@@ -0,0 +1,27 @@
+<nav>
+	<i class='bx bx-menu'></i>
+	<h3 style="color: white;">Welcome <span style="color: red;">Admin Dashboard</span></h3>
+	<form action="#">
+		<div class="form-input">
+
+		</div>
+	</form>
+
+	<a href="#" class="notification">
+		<i class='bx bxs-bell'></i>
+		<span class="num">8</span>
+	</a>
+	<a href="#" class="profile">
+		<img src="img/people.png">
+	</a>
+	<a href="">
+		<h1>Hi, <span style="color: red;"><?php echo $_SESSION['admin_name'] ?></span></h1>		
+	
+		<a href="logout.php" class="btn">logout</a>
+
+	</a>
+
+
+
+
+</nav>
