064d3b9a30589369050729cebe70c27d974087f2
Thimedha Viraj <153438859+Thimedha@users.noreply.github.com>
2024-05-17 02:14:13 +0530

    BMI And chat included

---
+++ chatbot/constants.js
@@ -0,0 +1,35 @@
+// Options the user could type in
+const prompts = [
+  ["hi", "hey", "hello"],
+  ["contact"],
+  ["body tone center"],
+  ["location"],
+  ["service"],
+
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["hi", "hey", "hello"],
+  ["contact detail is 077896325 , His name is nilanga"],
+  ["Body tone center is health and fitness body calory and fitness management center"],
+  ["This location is center of maharagama"],
+  ["service is providing schedule, body calory managingand fitnes equipment"],
+
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
