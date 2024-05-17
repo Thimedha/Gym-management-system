ded0aa2f4c5ffb980f1b46f9f9ffb4c0312e7668
Thimedha Viraj <153438859+Thimedha@users.noreply.github.com>
2024-05-17 14:26:19 +0530

    Add files via upload

---
+++ Instructor/Included/navContainer.php
@@ -0,0 +1,20 @@
+<nav>
+	<i class='bx bx-menu'></i>
+	<h3><span style="color: white;">Instructor Dashboard</span></h3>
+	<form action="#">
+		<div class="form-input">
+
+		</div>
+	</form>
+
+	<a href="#" class="notification">
+		<i class='bx bxs-bell'></i>
+		<span class="num">8</span>
+	</a>
+	<!-- <a href="#" class="profile">
+		<img src="img/people.png">
+	</a> -->
+	<a href="">
+		<h4><span style="color: red;"><?php echo $_SESSION['instructor_name'] ?></span></h4>		
+	
+</nav>
